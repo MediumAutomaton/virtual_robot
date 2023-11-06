85c3fd33ede9383f54d9389a916bb04cfea3cfbc
MediumAutomaton <michaelr@wordenhome.net>
2023-11-05 20:39:40 -0600

    Config file changes

--- Controller/src/virtual_robot/config/Config.java
+++ Controller/src/virtual_robot/config/Config.java
@@ -22,7 +22,7 @@ public class Config {
     /**
      *  Width of the field, in pixels
      */
-    public static final double FIELD_WIDTH = 648;
+    public static final double FIELD_WIDTH = 576; //default 648
 
     /**
      * Whether to use "Virtual Gamepad" (true -> Virtual gamepad, false -> Real gamepad)
@@ -32,7 +32,7 @@ public class Config {
     /**
      * The image object for the field.
      */
-    public static final Image BACKGROUND = new Image("/virtual_robot/assets/centerstage648.bmp");
+    public static final Image BACKGROUND = new Image("/virtual_robot/assets/centerstage576.bmp");
 
     /**
      * If true, Virtual Gamepad joysticks and triggers will stay in the position where they were
@@ -47,7 +47,8 @@ public class Config {
      * Define the game.  This must match the forGame attribute of the GameElementConfig annotation
      * on the VirtualGameElement implementations.
      */
-    public static final Game GAME = new CenterStage();
+//    public static final Game GAME = new CenterStage();
+    public static final Game GAME = new NoGame();
 
     /**
      * Friction coefficient between field and robot wheels. A very high value will minimize the effect
